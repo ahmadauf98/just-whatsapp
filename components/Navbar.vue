3100d9cab6bcb83e9971ffe540b7a321c3098f32
ahmadauf98 <ahmadauf98@gmail.com>
2020-07-15 18:58:45 +0800

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- As a heading -->
-  <b-navbar class="Navbar ml-auto" type="dark">
-    <b-navbar-brand tag="h1" class="mx-auto my-auto">
+  <b-navbar class="Navbar ml-auto" style="dark">
+    <b-navbar-brand class="text mx-auto my-auto h1" >
       <img
         src="~/static/brand.svg"
         alt="just whatsapp brand logo"
@@ -20,5 +20,9 @@ export default {}
 /* Global Scoped */
 .Navbar {
   background-color: #00473e;
+}
+
+.text{
+  color: #faae2b;
 }
 </style>
