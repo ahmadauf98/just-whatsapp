bfd15d27fa89a8cba5e90cbda1fea0ce921664c9
ahmadauf98 <ahmadauf98@gmail.com>
2020-07-16 01:13:55 +0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,17 @@
 <template>
+  <!-- Author: Ahmad Auf Bin Nasruddin -->
+  <!-- Developer: Ahmad Auf Bin Nasruddin -->
+  <!-- Github: https://github.com/ahmadauf98/just-whatsapp -->
+  <!-- DEV: https://dev.to/aufnasa_ -->
   <div>
     <Nuxt />
   </div>
 </template>
 
 <style>
-
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
