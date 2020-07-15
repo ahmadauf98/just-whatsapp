6d1adbd856abbdf56d2fe0cb1733527347506766
ahmadauf98 <ahmadauf98@gmail.com>
2020-07-15 20:19:22 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,24 @@
     <template>
       <IntroCard />
     </template>
+
+    <!--  -->
+    <template>
+      <Footer />
+    </template>
   </div>
 </template>
 
 <script>
 import Navbar from '~/components/Navbar.vue'
 import IntroCard from '~/components/IntroCard.vue'
+import Footer from '~/components/Footer.vue'
+
 export default {
   components: {
     Navbar,
-    IntroCard
+    IntroCard,
+    Footer,
   },
 }
 </script>
